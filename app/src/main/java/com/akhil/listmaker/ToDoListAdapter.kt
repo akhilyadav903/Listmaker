b69c57f845a680c4c87a22c3da8e0cd77688ca6f
Aadhit <aadhit@newslanded.com>
2021-07-25 23:46:59 +0530

    fourth change to github

--- app/src/main/java/com/akhil/listmaker/ToDoListAdapter.kt
+++ app/src/main/java/com/akhil/listmaker/ToDoListAdapter.kt
@@ -1,13 +1,14 @@
 package com.akhil.listmaker
 
 import android.view.ViewGroup
+import android.view.LayoutInflater
 import androidx.recyclerview.widget.RecyclerView
 
 class ToDoListAdapter: RecyclerView.Adapter<ToDoListViewHolder>() {
     private val toDolists = arrayOf("Android Devolopment, Housework, Errands")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoListAdapter {
-        val View - LayoutInflater.from(parent.context)
+        val View = LayoutInflater.from(parent.context)
         .inflate(R.layout.todo_list_view_holder, parent, false)
         return TodoListsViewHolder(View)
 
